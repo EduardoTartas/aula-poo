bd0251e6dcb7e5ce4a8f0249f34ad08c5befc45c
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 10:47:54 -0400

    Adicionando lógicas do encapsulamento na classe Conta.kt

--- tarefaBanco/src/Conta.kt
+++ tarefaBanco/src/Conta.kt
@@ -1,9 +1,9 @@
 class Conta(val numeroConta: String,val cliente:Cliente) {
 
-
+    // O saldo é privado para limitar o acesso somente à classe.
     private var saldo: Double = 0.0
 
-
+    // Métodos públicos que permitem a manipulação externa do saldo.
     fun depositar(valor: Double){
     }
 
