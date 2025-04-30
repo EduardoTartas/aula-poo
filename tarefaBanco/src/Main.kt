3587b19a8a89858404653541e8e22ac8533b6cf2
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 12:44:40 -0400

    Utilizando os métodos

--- tarefaBanco/src/Main.kt
+++ tarefaBanco/src/Main.kt
@@ -1,3 +1,37 @@
 fun main() {
-    println("Hello World!")
+    var banco = Banco("Tartas' Bank");
+    var agenciaUm = Agencia("agenciaUm");
+    var agenciaDois = Agencia("agenciaDois");
+    var clienteUm = Cliente("Eduardo");
+    var clienteDois = Cliente("Eduardo");
+    var contaUm = Conta("123", clienteUm);
+    var contaDois = Conta("321", clienteDois);
+
+
+    banco.adicionarAgencia(agenciaUm);
+    banco.adicionarAgencia(agenciaDois);
+    banco.removerAgencia(agenciaUm);
+    banco.obterAgencia("agenciaUm");
+
+    println("//-------------------------//")
+
+    agenciaUm.adicionarConta(contaUm);
+    agenciaDois.adicionarConta(contaDois);
+    agenciaUm.removerConta(contaDois);
+    agenciaDois.obterConta("contaDois");
+
+    println("//-------------------------//")
+
+    clienteUm.imprimirDetalhesCliente();
+    clienteUm.definirEndereco("123 Main Street");
+    clienteUm.imprimirDetalhesCliente();
+    clienteDois.imprimirDetalhesCliente();
+
+    println("//-------------------------//")
+
+    contaUm.obterSaldo();
+    contaUm.depositar(1.200);
+    contaUm.sacar(1.000);
+    contaDois.obterSaldo();
+    contaDois.sacar(4.000);
 }
