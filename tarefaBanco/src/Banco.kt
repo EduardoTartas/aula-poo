7c6c370d0425e8221ecf6dd68edb8e6397f4f044
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 11:28:03 -0400

    Implementando os métodos na classe Banco.kt

--- tarefaBanco/src/Banco.kt
+++ tarefaBanco/src/Banco.kt
@@ -1,17 +1,35 @@
 class Banco(val nome: String) {
+
     // A lista de agencias é privada para limitar o acesso somente à classe.
-    private var agencias: MutableList<Agencia> = mutableListOf() ;
+    private var agencias: MutableList<Agencia> = mutableListOf()
 
     // Métodos públicos que permitem a manipulação externa da lista de agencias.
-     fun adicionarAgencia(agencia: Agencia){
-        this.agencias.add(agencia);
+    fun adicionarAgencia(agencia: Agencia) {
+        if (this.agencias.none { it.nome == agencia.nome }) {
+            this.agencias.add(agencia)
+            println("Agencia ${agencia.nome} adicionada com sucesso!")
+        } else {
+            println("Agencia ${agencia.nome} já existe nesse banco!")
+        }
     }
 
-    fun removerAgencia(agencia: Agencia){
-        this.agencias.remove(agencia);
+    fun removerAgencia(agencia: Agencia) {
+        if (!this.agencias.none { it.nome == nome }) {
+            this.agencias.remove(agencia)
+            println("Agencia ${agencia.nome} removida com sucesso!")
+        } else {
+            println("Agencia ${agencia.nome} não existe nesse banco!")
+        }
     }
 
-    fun obterAgencia(nome: String): Agencia?{
-        return this.agencias.find{it.nome == nome};
+    fun obterAgencia(nome: String): Agencia? {
+        val agencia = this.agencias.find { it.nome == nome }
+        if (agencia != null) {
+            println("Agencia ${agencia.nome} encontrada com sucesso!")
+            return agencia
+        } else {
+            println("Agencia $nome não foi encontrada")
+        }
+        return agencia
     }
 }
