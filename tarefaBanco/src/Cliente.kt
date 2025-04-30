3e6c9369aa4dcbcfde43890937bab1eaa7d7b1fc
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 10:39:01 -0400

    Adicionando lógicas do encapsulamento na classe Cliente.kt

--- tarefaBanco/src/Cliente.kt
+++ tarefaBanco/src/Cliente.kt
@@ -1,9 +1,13 @@
-class Cliente(val idCliente: String,private val nome: String, private var endereco: String){
+class Cliente(val idCliente: String, val nome: String){
 
+    private lateinit var endereco: String
+
+    //função para obter o nome do cliente
     fun obterNome():String{
         return this.nome;
     }
 
+    //função publica para mudar o endereço do cliente
     fun definirEndereco(endereco: String){
         this.endereco = endereco;
     }
