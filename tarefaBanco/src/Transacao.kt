49f97e9acf85ea8c22ca43e8ba8293bfe5977d9f
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 12:12:39 -0400

    fix

--- tarefaBanco/src/Transacao.kt
+++ tarefaBanco/src/Transacao.kt
@@ -1,12 +1,18 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
-class Transacao(val valor: Double, val data: String) {
+class Transacao(private val valor: Double) {
     // IdTransacao é privado para limitar o acesso somente à classe.
     private val idTransacao: String = UUID.randomUUID().toString();
 
+    //Pega a data atual
+    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy - HH:mm:ss");
+    val data = LocalDateTime.now().format(formatter)
+
     // Método público que exibe as informações da transação feita.
     fun executar(operacao:String):Boolean{
-        println("Transação: $operacao, de valor ${this.valor} resgistrada com o ID ${this.idTransacao}" )
+        println("Transação: $operacao, de valor ${this.valor} registrada com o ID ${this.idTransacao} em $data" )
         return true;
     }
 }
