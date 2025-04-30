6f49213edf58da85485f9f0890f042958f161410
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 12:23:16 -0400

    Implementando os métodos na classe Conta.kt

--- tarefaBanco/src/Conta.kt
+++ tarefaBanco/src/Conta.kt
@@ -1,16 +1,38 @@
-class Conta(val numeroConta: String,val cliente:Cliente) {
-
+class Conta(val numeroConta: String, val cliente: Cliente) {
     // O saldo é privado para limitar o acesso somente à classe.
     private var saldo: Double = 0.0
 
     // Métodos públicos que permitem a manipulação externa do saldo.
-    fun depositar(valor: Double){
+    fun depositar(valor: Double) {
+        if (valor <= 0) {
+            println("Valor para depósito inválido.")
+            return
+        }
+        saldo += valor
+        println("Depósito realizado.")
+        val transacao = Transacao(valor)
+        transacao.executar("Depósito")
     }
 
-    fun sacar(valor: Double){
+    fun sacar(valor: Double) {
+        if (valor <= 0) {
+            println("Valor para saque inválido.")
+            return
+        }
+        if (saldo < valor) {
+            println("Saldo insuficiente para o saque.")
+            return
+        }
+        saldo -= valor
+        println("Saque realizado.")
+        val transacao = Transacao(valor)
+        transacao.executar("Saque")
     }
 
-    fun obterSaldo():Double{
-        return saldo;
+    fun obterSaldo(): Double {
+        println("$numeroConta - Saldo atual: $saldo")
+        val transacao = Transacao(saldo)
+        transacao.executar("Extrato")
+        return saldo
     }
 }
