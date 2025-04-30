655b4a85e4e43d26d832fcde2b120b4f5c77f571
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 10:48:58 -0400

    fix

--- tarefaBanco/src/Cliente.kt
+++ tarefaBanco/src/Cliente.kt
@@ -1,14 +1,14 @@
 class Cliente(val idCliente: String, val nome: String){
 
-    //Endereço é privado para limitar o acesso somente à classe.
+    // Endereço é privado para limitar o acesso somente à classe.
     private lateinit var endereco: String
 
-    //Método para obter o nome do cliente.
+    // Método para obter o nome do cliente.
     fun obterNome():String{
         return this.nome;
     }
 
-    //Método públicos que permite a manipulação externa do enderço.
+    // Método públicos que permite a manipulação externa do enderço.
     fun definirEndereco(endereco: String){
         this.endereco = endereco;
     }
